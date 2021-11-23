c458484c1336626bb58761a06ece22a2a8084040
Андрей <boyarov1995@yandex.ru>
2021-11-23 19:20:12 +0300

    if else

---
+++ Examples005/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите имя ");
+string name = Console.ReadLine();
+
+if (name == "Андрей"){
+    Console.WriteLine("Здарова БРО");
+} else {
+    Console.WriteLine("Привет");
+}
