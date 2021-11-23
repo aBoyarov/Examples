5667ec358d03be792389321d269db457a9b1cc2e
Андрей <boyarov1995@yandex.ru>
2021-11-23 19:47:51 +0300

    Вес максимальной гири

---
+++ Examples006/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите вес гири: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int d = Convert.ToInt32(Console.ReadLine());
+int e = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine("Вес максимальной гири = " + max + " кг");
+
